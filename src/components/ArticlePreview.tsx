7b0ea872ec0f914956c794c35f4ebcde30a2d21e
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-15 17:22:44 +0100

    Update second page

--- src/components/ArticlePreview.tsx
+++ src/components/ArticlePreview.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { Result } from '../uility/interfacesFetchData'
-
+import { Container, Row, Col } from'react-bootstrap';
 function ArticlePreview() {
     const [articleId, setArticleId] = useState('');
     const location = useLocation();
@@ -16,10 +16,10 @@ function ArticlePreview() {
         const fetchArticle = async () => {
             const url = `https://api.spaceflightnewsapi.net/v4/articles/${articleId}`
             try {
-                const response = await fetch(url);
+                const response = await fetch(url)
                 if (response.ok) {
-                    console.log('fetch ok');
-                    setArticle(await response.json());
+                    console.log('fetch ok')
+                    setArticle(await response.json())
                 }
             } catch (error) {
                 console.error(error)
@@ -28,14 +28,24 @@ function ArticlePreview() {
             }
         }
 
-        fetchArticle();
+        fetchArticle()
     }, [articleId])
 
     return (
         <>
-            <h1>{article?.title}</h1>
+        <Container>
+            <Row>
+                <Col xs={10} className='d-flex flex-column justify-content-center align-items-center text-white'>
+                    <h1>{article?.title}</h1>
+                    <div className='d-flex container-fluid'>
+                    <img src={article?.image_url} alt={article?.title} className='img-fluid mx-5' />
+                    </div>
+                    <p>{article?.summary}</p>
+                </Col>
+            </Row>
+        </Container>
         </>
     );
 }
 
-export default ArticlePreview;
+export default ArticlePreview
